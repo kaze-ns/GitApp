c9abb78261bdea800c6ddebb20f2a5fc43a85467
kaze-ns <jeremmyaraya@hotmail.com>
2023-10-24 15:53:19 -0300

    Sout2

--- app/src/main/java/com/example/gitapp/MainActivity.java
+++ app/src/main/java/com/example/gitapp/MainActivity.java
@@ -12,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
     }
 }
